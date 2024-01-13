b6b9d410067b2e8013f106bf96429f589ca1e572
Sofia Bargues <barguessofia@gmail.com>
2024-01-13 06:35:56 +0000

    taiwlnd

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,17 @@ function App() {
 
   return (
     <div className="max-w-md flex flex-col items-center justify-center py-8">
-      <h1 className="text-2xl font-semibold ">Stopwatch</h1>
+      <h1 className="text-3xl font-semibold ">Stopwatch</h1>
       <div className="text-xl font-semibold py-4">
         <span> {("0" + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
         <span> {("0" + Math.floor((time / 1000) % 60)).slice(-2)}: </span>
         <span> {("0" + ((time / 10) % 100)).slice(-2)}</span>
       </div>
-      <div className=" w-1/3 max-w-sm flex flex-row justify-evenly">
+      <div className=" w-1/3 max-w-sm flex flex-row justify-evenly ">
         {running ? (
           <button
-            className="border rounded-lg py-1 px-3.5"
+            className="btn btn-info"
+            // className="border rounded-lg py-5 px-3.5"
             onClick={() => {
               setRunning(false);
             }}
@@ -37,7 +38,7 @@ function App() {
           </button>
         ) : (
           <button
-            className="border rounded-lg py-1 px-3"
+            className="border rounded-lg p-10 py-1 px-3"
             onClick={() => {
               setRunning(true);
             }}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
