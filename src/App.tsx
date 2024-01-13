cbadbab10b83572bd9692a950be4118b191c9699
Sofia Bargues <barguessofia@gmail.com>
2024-01-13 07:02:57 +0000

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -18,42 +18,44 @@ function App() {
   }, [running]);
 
   return (
-    <div className="max-w-md flex flex-col items-center justify-center py-8 min-h-screen">
-      <h1 className="text-3xl font-semibold ">Stopwatch</h1>
-      <div className="text-xl font-semibold py-4">
-        <span> {("0" + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
-        <span> {("0" + Math.floor((time / 1000) % 60)).slice(-2)}: </span>
-        <span> {("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </div>
-      <div className=" w-1/3 max-w-sm flex flex-row justify-evenly ">
-        {running ? (
+    <div className=" flex flex-col items-center justify-center min-h-screen font-mono ">
+      <div className=" flex flex-col items-center justify-center gap-9 bg-[#2a323c] py-10 w-80 rounded-xl">
+        <h1 className="text-3xl font-semibold ">Stopwatch</h1>
+        <div className="text-5xl flex flex-row items-center font-semibold">
+          <span> {("0" + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
+          <span> {("0" + Math.floor((time / 1000) % 60)).slice(-2)}: </span>
+          <span> {("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <div className=" w-1/3 max-w-sm flex flex-row gap-4 justify-evenly ">
+          {running ? (
+            <button
+              className="btn btn-error"
+              onClick={() => {
+                setRunning(false);
+              }}
+            >
+              Stop
+            </button>
+          ) : (
+            <button
+              className="btn btn-success"
+              onClick={() => {
+                setRunning(true);
+              }}
+            >
+              Start
+            </button>
+          )}
           <button
-            className="btn btn-error"
+            className="btn btn-info"
+            disabled={running}
             onClick={() => {
-              setRunning(false);
+              setTime(0);
             }}
           >
-            Stop
+            Reset
           </button>
-        ) : (
-          <button
-            className="btn btn-success"
-            onClick={() => {
-              setRunning(true);
-            }}
-          >
-            Start
-          </button>
-        )}
-        <button
-          className="btn btn-info"
-          disabled={running}
-          onClick={() => {
-            setTime(0);
-          }}
-        >
-          Reset
-        </button>
+        </div>
       </div>
     </div>
   );
